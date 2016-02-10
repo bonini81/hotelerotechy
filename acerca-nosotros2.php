5a5cef8b5dcd57a43d7d3c544edaffd627abbaff
Andrés Domínguez <pugle81@gmail.com>
2016-02-09 22:53:55 -0500

    Created a index-test.php for github upload test

---
+++ acerca-nosotros2.php
@@ -0,0 +1,92 @@
+<?php
+$title ="Muchas Gracias!";
+include("includes/header.php");
+?>
+
+
+<!-- End Section Header style3 -->
+
+<!-- Start Pages Title Style1 -->
+
+<section id="Page-title" class="Page-title-Style1">
+	<div class="container inner-Pages">
+    	<div class="row">
+            <div class="Page-title">
+				<div class="col-md-6 Title-Pages">
+                	<h2>Gracias</h2>
+                </div>
+                <div class="col-md-6 Catogry-Pages">
+					<p>Usted está aquí:  <a href="#">Inicio</a> / Gracias</p>
+                </div>
+            </div>
+ 		</div>
+    </div>
+</section>
+
+<!-- End Pages Title Style1 -->
+
+<!-- Start Image Parallax Background  -->
+<!--
+<div class="Image-Parallaxer-Home parallax">
+	<div class="tp-banner-container">
+		<div class="tp-banner-image">
+			<ul>
+				<li data-transition="fade" data-slotamount="7">
+					<div class="SliderText text-center content-b rs-parallaxlevel-3 start">
+					<div class="caption lft icon-cont medium_text_shadow tp-resizeme" data-y="90" data-x="center" data-speed="400" data-start="2300"><h2>Do You Look For </h2></div>
+					<div class="caption sft slide-head large_text tp-resizeme" data-x="center" data-y="170" data-speed="400" data-start="3000">
+						<div class="text-scroll">
+							<h1 style="margin-top: 0px;">New Solutions for your Business</h1>
+							<h1>A Stunning Template for your Website</h1>
+							<h1>Very Helpful Support Team</h1>
+							<h1>Creative Solutions for your Company</h1>
+						</div>
+					</div>
+					<div class="caption sfb large-desc small_text  tp-resizeme" data-x="center" data-y="250" data-speed="400" data-start="3200"><div><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer lorem quam, adipiscing<br>condimentum tristique vel.</p></div></div>
+					<div class="caption sfb large-desc center tp-resizeme" data-x="center" data-y="350" data-speed="370" data-start="3400"><a href="#" class="btn Slicefive activelink">More Features</a>&nbsp;&nbsp;<a href="#" class="btn Slicefive">Purchase Now</a></div>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div> -->
+
+<!-- End Image Parallax Background -->
+
+
+
+<!-- Start TXT with Slider -->
+
+<section id="about" class="light-wrapper">
+	<div class="container inner">
+    	<div class="row">
+        	<div class="col-md-6">
+            	<div class="TextImg-Block">
+                	<h1>Gracias</h1>
+                    <div class="line-break"></div>
+                    
+                    <p>Muchas Gracias por contactarnos, nos comunicaremos lo más pronto posible con usted!</p>
+                   
+                </div>
+            </div>
+            
+
+		</div>
+    </div>
+</section>
+
+<!-- End TXT with Slider -->
+
+
+
+
+<br/>
+
+
+
+
+
+
+<?php
+include("includes/footer.php");
+?>
